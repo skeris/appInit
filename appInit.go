d59903dfc289e66e9665280b17f138efc2e12729
Skeris <kotilion.95@gmail.com>
2020-12-06 11:54:37 +0300

    initialize with some misunderstanding and without options

---
+++ appInit.go
@@ -0,0 +1,39 @@
+package appInit
+
+import (
+	"context"
+	"go.uber.org/zap"
+	"os"
+	"os/signal"
+	"time"
+)
+
+type AppConstructor =  func(ctx context.Context) (CommonApp, error)
+
+type CommonApp interface {
+	GetLogger() *zap.Logger
+	GetErr() chan error
+}
+
+func Initialize(constructor AppConstructor) {
+	defer time.Sleep(1500 * time.Millisecond)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sm, err := constructor(ctx)
+	if err != nil {
+		panic(err)
+	}
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	logger := sm.GetLogger()
+
+	select {
+	case <-stop:
+		logger.Info("Application was interrupted.")
+	case err := <-sm.GetErr():
+		logger.Panic("A fatal error occured", zap.Error(err))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/skeris/appInit
+
+go 1.15
